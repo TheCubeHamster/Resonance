744eb71b83bda68515c9949ad9639cc966aabb9d
Alex Xie <38799004+TheCubeHamster@users.noreply.github.com>
2025-04-19 11:09:02 -0700

    Initial Commit

---
+++ Taptronome/TaptronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaptronomeApp.swift
+//  Taptronome
+//
+//  Created by Alex Xie on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TaptronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaptronomeTests/TaptronomeTests.swift
@@ -0,0 +1,17 @@
+//
+//  TaptronomeTests.swift
+//  TaptronomeTests
+//
+//  Created by Alex Xie on 4/19/25.
+//
+
+import Testing
+@testable import Taptronome
+
+struct TaptronomeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
