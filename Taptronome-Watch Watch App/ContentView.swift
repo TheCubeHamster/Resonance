be1f7bc4dfee5473ae99f0a350bb08d67863f419
Jordan Wong <jordanhwong1@gmail.com>
2025-04-19 14:16:59 -0700

    Basic UI for phone and watch

---
+++ Taptronome-Watch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taptronome-Watch Watch App
+//
+//  Created by Jordan Wong on 4/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Taptronome-Watch Watch App/Taptronome_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Taptronome_WatchApp.swift
+//  Taptronome-Watch Watch App
+//
+//  Created by Jordan Wong on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Taptronome_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
